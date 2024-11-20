cc787df013c0fed76155cb11ba2ed97593658c94
Daisy Nardella <daisy50@live.it>
2024-11-20 22:36:40 +0100

    primo commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let num1 = 10;
+console.log(num1);
+
+let num2 = 5;
+console.log(num2);
+
+console.log("Il valore della variabile num1 e' "+ num1)
+console.log("Il valore della variabile num2 e' "+ num2)
+
+let stringa = `Ciao a tutti`;
+console.log("Il valore della stringa e' "+ stringa)
+stringa= `Ciao prof ti auguro una bella giornata`
+console.log(stringa)
+console.log("Il valore della stringa e' "+ stringa)
+
+const PIGRECO=3.14159265359;
+console.log("Il valore di PIGRECO e'" +PIGRECO);
+PIGRECO=3.9876   
+
+// va in errore perchè le non si può riassegnare una costante
